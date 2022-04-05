f1d274338adb616fe2c5a85fbc7e271234634d2a
Mariaedoarda <maria.moreira115@etec.sp.gov.br>
2022-04-05 12:03:33 -0300

    modificação

---
+++ pessoa.class.php
@@ -0,0 +1,22 @@
+<?
+
+Class Pessoa{
+     private $nome;
+     private $endereco;
+
+     public function setNome($nome){
+
+         $this->nome = $nome;
+     }
+
+     public function getNome(){
+
+         return $this->nome;
+     }
+
+     public function getEndereco(){
+
+        return $this->endereco;
+     }
+
+}
